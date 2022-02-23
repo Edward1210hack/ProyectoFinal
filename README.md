51b686924ef657e71c90587a55138937f6b623ce
edward <edward.1210@hotmail.com>
2022-02-22 23:12:11 -0500

    proyecto final

---
+++ README.md
@@ -0,0 +1,27 @@
+# LU MAKEUP
+
+## Descripción
+
+Proyecto final E-commerce de Maquillaje para el curso de React en Coder House.
+
+**Tecnologías y librerías:**
+
+- React JS
+- React hooks
+- React-Router
+- Sass
+- Firebase para base de datos
+
+## Dependencias
+
+- React-Router
+- Firebase
+- Font Awesome
+- Sass
+- React Awesome Reveal
+- query-string
+- react-hook-form
+- sass
+
+
+ 
---
+++ src/utils/images.js
@@ -0,0 +1,21 @@
+import producto1 from "../assets/producto1.jpg";
+import producto2 from "../assets/producto2.jpg";
+import producto3 from "../assets/producto3.jpg";
+import producto4 from "../assets/producto4.jpg";
+import producto5 from "../assets/producto5.jpg";
+import producto6 from "../assets/producto6.jpg";
+import producto7 from "../assets/producto7.jpg";
+import logo from "../assets/logo.png";
+import producto9 from "../assets/producto.jpg";
+
+export const Images = {
+  producto1,
+  producto2,
+  producto3,
+  producto4,
+  producto5,
+  producto6,
+  producto7,
+  Logo,
+  producto8,
+};
